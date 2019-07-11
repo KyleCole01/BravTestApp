919da7b9a0870c69cc5fda3a104a028d5266cde7
ThaDocIzN <rodan72@msn.com>
2019-07-11 01:22:47 -0500

    created user repo

---
+++ app/src/main/java/com/thadocizn/brav/repositories/UserRepository.kt
@@ -0,0 +1,20 @@
+package com.thadocizn.brav.repositories
+
+import android.arch.lifecycle.MutableLiveData
+import com.thadocizn.brav.model.User
+
+/**
+ * Created by charles on 10,July,2019
+ */
+class UserRepository(private val user: User) {
+
+    lateinit var users: MutableList<User>
+    private val mutableLiveData = MutableLiveData<List<User>>()
+
+    val userList: MutableLiveData<List<User>>
+        get() {
+            users.add(user)
+            mutableLiveData.postValue(users)
+            return mutableLiveData
+        }
+}
