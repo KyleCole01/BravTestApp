362ea750b9dac566ffcb703a22ffa0f16a4a4f09
ThaDocIzN <rodan72@msn.com>
2019-07-11 13:09:58 -0500

    added snack bar to test functionality

--- app/src/main/java/com/thadocizn/brav/views/UserRegister.kt
+++ app/src/main/java/com/thadocizn/brav/views/UserRegister.kt
@@ -3,11 +3,16 @@ package com.thadocizn.brav.views
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.thadocizn.brav.R
+import kotlinx.android.synthetic.main.activity_user_register.*
+import org.jetbrains.anko.design.snackbar
 
 class UserRegister : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_register)
+        btnGo.setOnClickListener{
+            it.snackbar("Hello There")
+        }
     }
 }
